f6cbd69a9e852bba914aa4eb84215b8cc855f52b
Vini Queiroz <viniderp@gmail.com>
2021-07-14 16:45:57 -0300

    item 2

---
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-2/exercicios2-3.test.js
@@ -0,0 +1,14 @@
+const { users, getUserName, findUserById } = require('./exercicios2-3.js');
+
+describe('function getUserName', () => {
+  test('user must be found', () => {
+    // expect.assertions(1);
+    return expect(getUserName('4')).resolves.toEqual('Mark');
+  });
+
+  test('user must not be found', () => {
+    return expect(getUserName('7')).rejects.toEqual({
+      error: 'User with ' + '7' + ' not found.',
+    });
+  });
+});
